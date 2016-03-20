82046f72d08360782b3e25a5196cc3eab8ec7c2d
Jason <zhanyouwei@meitunmama.com>
2016-03-20 09:11:55 +0800

    更新扩展

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-two-dimension-code-extension
-============================
+# chrome-qrcode-extension
 
 chrome浏览器网页URL生成二维码插件
+
+## 快捷键 
+ - Windows `Alt+Q`  
+ - Mac `Option+Q`
--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,14 @@
 /**
  * Created by Mr.zhan on 14-10-16.
- * E-mail:mrzhanyouwei@163.com
+ * E-mail:zhanyouwei@icloud.com
  */
-$(function(){
+$(function () {
 
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        width : 200,
-        height : 200
-    });
-    chrome.tabs.getSelected(function (tab){
-        console.log(tab);
-        qrcode.makeCode(tab.url);
-    });
+	var qrcode = new QRCode(document.getElementById("qrcode"), {
+		width: 200,
+		height: 200
+	});
+	chrome.tabs.getSelected(function (tab) {
+		qrcode.makeCode(tab.url);
+	});
 });
